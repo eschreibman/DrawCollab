a46e906891672d838e68db35d73fe68f25cd2fb0
Nicki Thomson <nicki.r.thomson@gmail.com>
2016-10-28 11:18:02 -0400

    Basic option parsing for server

---
+++ server.py
@@ -0,0 +1,29 @@
+# Initial server goals:
+#   Accept requests
+#   Server infomation
+
+import SimpleHTTPServer
+import SocketServer
+import getopt, sys
+
+def main():
+    
+    port = 80
+    host = "localhost"
+
+    try:
+        options, args = getopt.getopt(sys.argv[1:], 'p:')
+        port_selection = filter(lambda x: "-p" in x, options)
+        if len(port_selection) > 0:
+            port = port_selection[0][1]
+    except (getopt.GetoptError, IndexError):
+        usage()
+        return
+
+    return
+
+def usage():
+    print "-p PORT_NUMBER, port to run server on (defaults to 80)"
+
+if __name__ == '__main__':
+    main()
