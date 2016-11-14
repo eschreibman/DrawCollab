5d05461dacc7bf0b8ddcaee02365f62c0575c2f2
NickiThomson <nicki.r.thomson@gmail.com>
2016-11-14 12:49:23 -0500

    Fixed server2 so it reads GET request

--- server2.py
+++ server2.py
@@ -1,19 +1,44 @@
+#A server to run draw collab
 import socket
+import SimpleHTTPServer
+import SocketServer
+import getopt, sys
 
-s = socket.socket()
-host = "localhost" #socket.gethostname()
-port = 80
+def main():
+    
+    port = 80
+    host = "localhost"
 
-s.bind((host, port))
-f = open('index.html', 'rb')
-s.listen(5)
+    try:
+        options, args = getopt.getopt(sys.argv[1:], 'p:')
+        port_selection = filter(lambda x: "-p" in x, options)
+        if len(port_selection) > 0:
+            port = int(port_selection[0][1])
+    except (getopt.GetoptError, IndexError):
+        usage()
+        return
 
-while True:
-    c, addr = s.accept()
-    print("Accepted a client")
-    c.send("HTTP/1.1 200 OK \n Content-type:text/html \n\n")
-    data = f.read(1024)
-    while data != "":
+
+    s = socket.socket()
+    host = "localhost" #socket.gethostname()
+
+    s.bind((host, port))
+    f = open('allInOne.html', 'rb')
+    s.listen(5)
+
+    while True:
+        c, addr = s.accept()
+        print("Accepted a client")
+        c.recv(1000)
+        c.send("HTTP/1.1 200 OK \n Content-type:text/html \n\n")
+        data = f.read()
         c.send(data)
-        data = f.read(1024)
-    f.seek(0,0)
+
+        f.seek(0,0)    
+        c.close()
+
+def usage():
+    print "-p PORT_NUMBER, port to run server on (defaults to 80)"
+
+if __name__ == '__main__':
+    main()
