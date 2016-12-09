a374afcfec0081380e17473554bb5e8edcc624c9
NickiThomson <nicki.r.thomson@gmail.com>
2016-12-08 22:34:00 -0500

    Restored drawing board

--- drawing_board.py
+++ drawing_board.py
@@ -3,14 +3,12 @@ from position_class import position
 class board:
     possibleUsers = ["^", "&", "%", "$"]
     anyUser = "@"
-    emptySpace = "_"
 
     def __init__(self, w, h):
         #create a totally empty board
         self.width = w
         self.height = h
-        self.theboard = [[self.emptySpace for x in range(w)] for y in range(h)]
-        self.userPosition = position(0, 0)
+        self.theboard = [['_' for x in range(w)] for y in range(h)]
 
     def addUser(self, userNum):
         #called by client
@@ -20,13 +18,8 @@ class board:
             self.user = self.possibleUsers[userNum]
         else:
             self.user = self.possibleUsers[len(self.possibleUsers) - 1]
-        self.theboard[self.userPosition.x][self.userPosition.y] = self.user
-        
-
-    def addUserServer(self):
-        #called by server
-        self.userNum = -1
-        self.user = self.anyUser
+        self.theboard[0][0] = self.user
+        self.userPosition = position(0, 0)
 
     def printBoard(self):
         for i in range(self.height):
@@ -40,7 +33,7 @@ class board:
         oldPosX = self.userPosition.x
         oldPosY = self.userPosition.y
         #clear where the user cursor used to be
-        self.theboard[oldPosX][oldPosY] = self.emptySpace
+        self.theboard[oldPosX][oldPosY] = '_'
         #update user position
         self.userPosition.x += dir.x
         self.userPosition.y += dir.y
@@ -69,16 +62,13 @@ class board:
             string += "["
             #width one less time to not put a comma at the end
             for j in range(self.width - 1):
-                #if(self.theboard[i][j] == self.user):
-                #    string += self.anyUser
-                #else:
                 string += self.theboard[i][j]
                 string += ","
             string += self.theboard[i][self.width - 1]
             string += "]"
         return string
 
-    def stringToBoard(self, str):
+    def stringToBoardFromServer(self, str):
         slen = len(str)
         k = 0
         for i in range(self.height):
@@ -87,47 +77,13 @@ class board:
                         while(str[k] == "[" or str[k] == "," or str[k] == "]"):
                                 k += 1
                         #deal with two users being on the same spot
-                        #if(str[k] == self.anyUser):
-                        #    self.theboard[i][j] = self.user
+                        # if(str[k] == self.anyUser):
+                        #     self.theboard[i][j] = self.user
                         if(k == slen):
                             return -1
                         self.theboard[i][j] = str[k]
                         k += 1
         return 0
     
-
     def update_user_token(self, user_token):
         self.user_token = user_token
-
-    def mergeBoards(self, otherBoard):
-        if((self.height != otherBoard.height) or (self.width != otherBoard.width)):
-            return -1
-        for i in range(self.height):
-            for j in range(self.width):
-                if(self.hasUser(i, j)):
-                    if(self.sameBoard(otherBoard)):
-                        self.theboard[i][j] = otherBoard.theboard[i][j]
-                else:
-                    self.theboard[i][j] = otherBoard.theboard[i][j]
-    
-    def hasUser(self, i, j):
-        for k in self.possibleUsers:
-            if(self.theboard[i][j] == k):
-                return True
-        return False
-
-    def findMyUser(self):
-        for i in range(self.height):
-            for j in range(self.width):
-                if(self.hasUser(i, j)):
-                    self.user = self.theboard[i][j]
-
-    def sameBoard(self, otherBoard):
-        if(self.user == otherBoard.user):
-            return True
-        return False
-
-
-
-
-
